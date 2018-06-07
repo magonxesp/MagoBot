03bca364bec862800cb8493fef47a497653f2933
MagonxESP <janma.360@gmail.com>
2018-06-07 17:53:23 +0200

    cambio de api en el bot y comando roll

--- magobot.py
+++ magobot.py
@@ -1,24 +1,10 @@
-import telebot  # bot api
-import rule34  # the magic is here
-import random  # memes
+from telegram.ext import Updater, CommandHandler
+import rule34
+import random
 import sys
 
-token = "593801508:AAF0qCsRxbyKG0I-QSCoh4wwW7A-G6HuccU"
 
-bot = telebot.TeleBot(token)
-
-
-def get_message_arg(message_text):
-    message_array = message_text.split(' ')[1:]
-
-    arg = ""  # message argument string
-    for message in message_array:
-        arg += message + ' '
-
-    return arg
-
-
-def get_image_by_tag(tag):
+def get_rule34_post(tag):
     try:
         images_urls = rule34.getImageURLS(tag)
 
@@ -33,21 +19,50 @@ def get_image_by_tag(tag):
     return None
 
 
-@bot.message_handler(commands=['rule34'])
-def send_post(message):
-    arg = get_message_arg(message.text)
-    bot.send_sticker(message.chat.id, "CAADAwAD0RAAAsKphwW8SGCoG75C8AI") # sticker de kanna buscando
-    image_url = get_image_by_tag(arg)
+def get_message_arg(message_text):
+    message_array = message_text.split(' ')[1:]
+
+    arg = ""  # message argument string
+    for message in message_array:
+        arg += message + ' '
+
+    arg = arg.rstrip()
+    return arg
+
+
+def send_rule34(bot, update):
+    arg = get_message_arg(update.message.text)
+    bot.send_sticker(update.message.chat_id, "CAADAwAD0RAAAsKphwW8SGCoG75C8AI")  # sticker de kanna buscando
+    image_url = get_rule34_post(arg)
 
     if image_url is not None:
-        bot.send_message(message.chat.id, image_url)
+        bot.send_message(update.message.chat_id, image_url)
     else:
-        bot.send_message(message.chat.id, "Prueba con otra cosa...")
+        bot.send_message(update.message.chat_id, "Prueba con otra cosa...")
 
 
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.send_message(message.chat.id, "Hola k ase")
+def send_welcome(bot, update):
+    bot.send_message(update.message.chat_id, "Hola k ase")
 
 
-bot.polling()
+def roll(bot, update):
+    max_random_value = 1000
+    arg = get_message_arg(update.message.text)
+
+    if arg.isnumeric():
+        max_random_value = int(arg)
+
+    value = random.randint(0, max_random_value)
+    bot.send_message(chat_id=update.message.chat_id, text=value, reply_to_message_id=update.message.message_id)
+
+
+# crea el updater del bot mediante el token del bot
+updater = Updater("593801508:AAF0qCsRxbyKG0I-QSCoh4wwW7A-G6HuccU")
+
+# añade los comandos al bot
+updater.dispatcher.add_handler(CommandHandler('start', send_welcome))
+updater.dispatcher.add_handler(CommandHandler('rule34', send_rule34))
+updater.dispatcher.add_handler(CommandHandler('roll', roll))
+
+updater.start_polling()
+updater.idle()
