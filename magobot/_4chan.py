6b87a303cf78bd01d258d79883ca40f2a71f8003
MagonxESP <janma.360@gmail.com>
2018-06-20 22:21:30 +0200

    funciones de comandos de 4chan

--- magobot/_4chan.py
+++ magobot/_4chan.py
@@ -1,23 +1,48 @@
-from .helper import send
+from .helper import send, random_element
 from basc_py4chan import Board
 from basc_py4chan import Thread
-from basc_py4chan import Post
-import random
 
 
-def get_4chan_random_thread_from_board(board):
-    pass
+def generator_to_array(generator) -> list:
+    array = list()
+
+    # el bucle no para hasta que el generador no de mas elementos
+    for element in generator:
+        array.append(element)
+
+    return array
 
 
-def get_4chan_random_post_from_thread(thread):
-    pass
+def get_threads(board_name):
+    threads = list()
+
+    board = Board(board_name)
+    threads_ids = board.get_all_thread_ids()
+
+    for thread_id in threads_ids:
+        threads.append(Thread(board, thread_id))
+
+    return threads
+
+
+def random_thread(thread_array):
+    thread = random_element(thread_array)
+    thread.update()
+    return thread
 
 
 def send_4chan_anime_wallpaper(bot, update):
-    board = Board('w')
-    threads_ids = board.get_all_thread_ids()
-    index = random.randint(0, len(threads_ids))
-    thread = Thread(board, threads_ids[index])
-    thread.update()
-    file = thread.file_objects()
-    send(bot, update, file.file_url)
+    thread = random_thread(get_threads('w'))
+    files = generator_to_array(thread.files())
+    send(bot, update, random_element(files))
+
+
+def send_4chan_random(bot, update):
+    thread = random_thread(get_threads('b'))
+    send(bot, update, thread.url)
+
+
+def send_4chan_ecchi(bot, update):
+    thread = random_thread(get_threads('e'))
+    files = generator_to_array(thread.files())
+    send(bot, update, random_element(files))
