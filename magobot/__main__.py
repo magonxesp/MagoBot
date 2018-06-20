74dbf59f0d8f47cc2541ba6eafd551863bfb30ec
MagonxESP <janma.360@gmail.com>
2018-06-20 22:19:33 +0200

    añade el comando random, randomw y ecchi

--- magobot/__main__.py
+++ magobot/__main__.py
@@ -1,7 +1,7 @@
 from telegram.ext import Updater, CommandHandler
 from .rule34 import send_rule34
 from .dummy import send_welcome, roll
-from ._4chan import send_4chan_anime_wallpaper
+from ._4chan import send_4chan_anime_wallpaper, send_4chan_random, send_4chan_ecchi
 
 # crea el updater del bot mediante el token del bot
 updater = Updater("593801508:AAF0qCsRxbyKG0I-QSCoh4wwW7A-G6HuccU")
@@ -10,7 +10,9 @@ updater = Updater("593801508:AAF0qCsRxbyKG0I-QSCoh4wwW7A-G6HuccU")
 updater.dispatcher.add_handler(CommandHandler('start', send_welcome))
 updater.dispatcher.add_handler(CommandHandler('rule34', send_rule34))
 updater.dispatcher.add_handler(CommandHandler('roll', roll))
-updater.dispatcher.add_handler(CommandHandler('random_wallpaper', send_4chan_anime_wallpaper))
+updater.dispatcher.add_handler(CommandHandler('random', send_4chan_random))
+updater.dispatcher.add_handler(CommandHandler('randomw', send_4chan_anime_wallpaper))
+updater.dispatcher.add_handler(CommandHandler('ecchi', send_4chan_ecchi))
 
 updater.start_polling()
 updater.idle()
