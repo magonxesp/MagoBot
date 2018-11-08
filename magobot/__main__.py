f68b2c33a7e001ab492d58435a040a386f0fe65f
MagonxESP <janma.360@gmail.com>
2018-11-08 21:48:44 +0100

    rename methods

--- magobot/__main__.py
+++ magobot/__main__.py
@@ -5,13 +5,13 @@ import magobot.commands
 bot = MagoBot(TOKEN)
 
 # Add command listeners
-bot.add_command(magobot.commands.Start())
-bot.add_command(magobot.commands.Roll())
-bot.add_command(magobot.commands.Rule34())
-bot.add_command(magobot.commands.RandomAnimeWallpaper())
-bot.add_command(magobot.commands.RandomBThread())
-bot.add_command(magobot.commands.RandomEcchiThread())
-bot.add_command(magobot.commands.RandomHentaiThread())
+bot.add_handler(magobot.commands.Start())
+bot.add_handler(magobot.commands.Roll())
+bot.add_handler(magobot.commands.Rule34())
+bot.add_handler(magobot.commands.RandomAnimeWallpaper())
+bot.add_handler(magobot.commands.RandomBThread())
+bot.add_handler(magobot.commands.RandomEcchiThread())
+bot.add_handler(magobot.commands.RandomHentaiThread())
 
 # start bot
 bot.start()
