5161ed0fb1bb5b30156ebba0eeedd8f61e86da77
MagonxESP <janma.360@gmail.com>
2018-11-08 21:32:28 +0100

    generic bot response

--- magobot/magobot.py
+++ magobot/magobot.py
@@ -1,4 +1,4 @@
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, RegexHandler
 from telegram import Chat, ParseMode
 
 
@@ -15,48 +15,75 @@ class MagoBot(object):
         self.__updater.idle()
 
 
+class ResponseType(object):
+    STICKER = 'sticker'
+    TEXT = 'text'
+
+
+class BotResponse(object):
+
+    def __init__(self, bot, update):
+        self.__bot = bot
+        self.__update = update
+        self.send_reply = False
+        self.send_mention = False
+        self.parse_mode = None
+        self.__message = None
+        self.__response_type = ResponseType.TEXT
+
+    def __prepare_params(self):
+        chat_type = self.__update.message.chat.type
+        chat_id = self.__update.message.chat.id
+        message_id = self.__update.message.message_id
+
+        params = {'chat_id': chat_id}
+
+        if chat_type == Chat.GROUP or chat_type == Chat.SUPERGROUP:
+            if self.send_reply:
+                params['reply_to_message_id'] = message_id
+            elif self.send_mention:
+                user = self.__update.message.from_user
+                markdown = "[@{}](tg://user?id={}) " + self.__message
+                self.__message = markdown.format(user.username, user.id)
+                params['parse_mode'] = ParseMode.MARKDOWN
+
+        return params
+
+    def send(self, message_type, message_content):
+        self.__message = message_content
+        params = self.__prepare_params()
+
+        if message_type == ResponseType.TEXT:
+            self.__bot.send_message(text=self.__message, **params)
+        elif message_type == ResponseType.STICKER:
+            self.__bot.send_sticker(sticker=self.__message, **params)
+
+
 class Command(CommandHandler):
 
     _command = ''
-
-    " Only when command is executed on group "
-    _send_reply = False
-    _send_mention = False
-
-    __bot = None
-    __update = None
+    _response = None
 
     def __init__(self):
         super().__init__(self._command, self.__execute, pass_args=True)
 
     def __execute(self, bot, update, args):
-        self.__bot = bot
-        self.__update = update
-
-        messages = self._on_execution(args)
-
-        if type(messages) is not 'list':
-            messages = [messages]
-
-        for message in messages:
-            self.__send(message)
-
-    def __send(self, message_text):
-        chat = self.__update.message.chat
-        message = self.__update.message
-
-        if chat.type == Chat.GROUP or chat.type == Chat.SUPERGROUP:
-            if self._send_reply:
-                self.__bot.send_message(chat_id=chat.id, text=message_text, reply_to_message_id=message.message_id)
-            elif self._send_mention:
-                user = message.from_user
-                markdown = "[@{}](tg://user?id={}) " + message_text
-                message_text = markdown.format(user.username, user.id)
-                self.__bot.send_message(chat_id=chat.id, text=message_text, parse_mode=ParseMode.MARKDOWN)
-            else:
-                self.__bot.send_message(chat_id=chat.id, text=message_text)
-        else:
-            self.__bot.send_message(chat.id, message_text)
+        self._response = BotResponse(bot, update)
+        self._on_execution(args)
 
     def _on_execution(self, args):
+        raise NotImplementedError()
+
+
+class Answer(RegexHandler):
+
+    pattern = ''
+
+    def __init__(self):
+        super().__init__(self.pattern, self.__trigger)
+
+    def __trigger(self):
+        pass
+
+    def _on_answer(self):
         raise NotImplementedError()
